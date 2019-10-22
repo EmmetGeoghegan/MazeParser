bf12e79b86863798b7a34d5a1b4023d3e14495b7
Emmet Geoghegan <46162302+EmmetGeoghegan@users.noreply.github.com>
2019-10-22 22:59:22 +0100

    Removing bad Visualisation Methods
    Removed Visualisation algorithms that did not yelid a good representation
    
    ie plots that had overlapping lines, lack of customisation and overlapping nodes
    
    Settled for a networkx planar_layout representation

--- display_graph.py
+++ display_graph.py
@@ -1,20 +1,8 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
-from graphviz import Digraph
-# from anytree import Node, RenderTree
-# from anytree.exporter import DotExporter
 
 
-# Graph display function basic implimentation to check graph logic is sound
-def draw_graph_mplib(nodes, connections):
-    # Init graph object
-    G = nx.MultiDiGraph()
-    # Add our nodes
-    G.add_nodes_from([i.name for i in nodes])
-    print(len(nodes))
-    # Add our edges
-    G.add_edges_from(connections)
 
     # Labeling and coloring graph
     node_colors = ["green"]+["lightblue"]*(len(nodes)-2) + ["red"]
@@ -28,39 +16,12 @@ def draw_graph_mplib(nodes, connections):
     plt.show()
 
 
-def draw_graphviz(nodes, connections):
-    dot = Digraph(comment="Maze", format="png")
-    for i in nodes:
-        print(i.name)
-        dot.node(str(i.name), "TEst")
-    dot.edges(connections)
-    dot.render("test.png", view=True)
+    node_colors = ["green"]+["skyblue"]*(len(G.nodes())-2) + ["red"]
 
+    carac = pd.DataFrame({'ID': G.nodes(), 'myvalue': node_colors})
+    carac = carac.set_index('ID')
+    carac = carac.reindex(G.nodes())
 
-# def draw_networkx(connections):
-#     con_from = []
-#     con_to = []
-#     for i in connections:
-#         con_from.append(i[0])
-#         con_to.append(i[1])
-#     df = pd.DataFrame({"from": con_from, "to": con_to})
-#
-#     G = nx.from_pandas_edgelist(df, "from", "to")
-#     # Fruchterman Reingold
-#     nx.draw(G, with_labels=True, node_size=3, node_color="skyblue", pos=nx.nx_agraph.graphviz_layout(G, prog="neato"))
-#     plt.title("fruchterman_reingold")
-#     plt.show()
-#     # pip install pygraphviz
-#     # Fruchterman Reingold
-#     nx.draw(G, with_labels=True, node_size=1, node_color="skyblue", layout=layout_reingold_tilford)
-#     plt.title("fruchterman_reingold")
-#     plt.show()
-#     # pip install pygraphviz
-
-
-def render_tree(nodes):
-    start = Node("start")
-    for i in nodes:
-        Node(i.name, parent=i.PrvNode.name)
-
-    print(RenderTree(start))
+    nx.draw(G, pos=pos, node_size=100, node_color=carac['myvalue'], with_labels=True)  # , node_color="skyblue")
+    plt.title("Graph Representation")
+    plt.show()
