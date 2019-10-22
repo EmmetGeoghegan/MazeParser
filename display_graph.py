4789b302985cf3b07596b24c111b38148107ee98
Emmet Geoghegan <46162302+EmmetGeoghegan@users.noreply.github.com>
2019-10-22 15:46:39 +0100

    Added render_tree and networkx drawers

--- display_graph.py
+++ display_graph.py
@@ -1,7 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import pandas as pd
 from graphviz import Digraph
+from anytree import Node, RenderTree
+from anytree.exporter import DotExporter
 
 
 # Graph display function basic implimentation to check graph logic is sound
@@ -33,8 +35,10 @@ def draw_graph_mplib(nodes, connections):
         labels[index] = i.name
 
     # Create and display the graph
-    nx.draw(G, node_color=node_colors, labels=labels, with_labels=True)
-    plt.show()
+    for i in range(10000,100000000000,10000):
+        print(i)
+        nx.draw(G, node_color=node_colors, labels=labels, with_labels=True, figsize=[i,i])
+        plt.show()
 
 
 def draw_graphviz(nodes, connections):
@@ -46,3 +50,30 @@ def draw_graphviz(nodes, connections):
     dot.render("test.png", view=True)
 
 
+def draw_networkx(connections):
+    con_from = []
+    con_to = []
+    for i in connections:
+        con_from.append(i[0])
+        con_to.append(i[1])
+    df = pd.DataFrame({"from": con_from, "to": con_to})
+
+    G = nx.from_pandas_edgelist(df, "from", "to")
+    # # Fruchterman Reingold
+    # nx.draw(G, with_labels=True, node_size=3, node_color="skyblue", pos=nx.nx_agraph.graphviz_layout(G, prog="neato"))
+    # plt.title("fruchterman_reingold")
+    # plt.show()
+    # pip install pygraphviz
+    # Fruchterman Reingold
+    nx.draw(G, with_labels=True, node_size=1, node_color="skyblue", layout=layout_reingold_tilford)
+    plt.title("fruchterman_reingold")
+    plt.show()
+    # pip install pygraphviz
+
+
+def render_tree(nodes):
+    start = Node("start")
+    for i in nodes:
+        Node(i.name, parent=i.PrvNode.name)
+
+    print(RenderTree(start))
